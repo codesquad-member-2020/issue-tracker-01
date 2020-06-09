268f34e812f4b9126a23700a2aef689b2f5be7b7
haveagood <haveagood@naver.com>
2020-06-09 20:41:42 +0900

    BE-#13 feat : Controller에 Api 메소드 추가
    - Swagger Api tag 추가
    - Github에서 인증을 받아 사용자 정보를 가져오는 oauthAuthentication 메소드 추가
    - 로그인을 했을 때, jwt토큰을 비교하여 로그인 정보를 확인하는 loginWithGithub 메소드 추

--- BE/src/main/java/kr/codesquad/issuetracker/controller/LoginController.java
+++ BE/src/main/java/kr/codesquad/issuetracker/controller/LoginController.java
@@ -1,4 +1,27 @@
 package kr.codesquad.issuetracker.controller;
 
+import io.swagger.annotations.Api;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "Login")
+@RestController
+@RequestMapping("/login")
 public class LoginController {
+
+    private static final Logger log = LoggerFactory.getLogger(LoginController.class);
+
+    @GetMapping
+    public ResponseEntity<String> loginWithGithub() {
+        return new ResponseEntity<>();
+    }
+
+    @GetMapping("/oauth")
+    public ResponseEntity<String> oauthAuthentication() {
+        return new ResponseEntity<>();
+    }
 }
