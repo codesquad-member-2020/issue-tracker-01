36ada253d7b00a472d6321ba6f2828d8f352275d
IdionKim <ksundong@gmail.com>
2020-06-10 22:08:38 +0900

    BE-#18 feat: Swagger 설정 추가

--- BE/src/main/java/kr/codesquad/issuetracker/controller/IssueController.java
+++ BE/src/main/java/kr/codesquad/issuetracker/controller/IssueController.java
@@ -1,5 +1,7 @@
 package kr.codesquad.issuetracker.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import kr.codesquad.issuetracker.controller.response.IssueResponse;
 import kr.codesquad.issuetracker.domain.Issue;
 import kr.codesquad.issuetracker.service.IssueService;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Api(value = "이슈에 관련된 API")
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/issues")
@@ -19,6 +22,7 @@ public class IssueController {
 
     private final IssueService issueService;
 
+    @ApiOperation(value = "이슈 목록 조회")
     @GetMapping("")
     public IssueResponse showIssues() {
         List<Issue> issues = issueService.findIssues();
