8edcf1ea36ec3ac7cf0534c05c83000529128480
haveagood@naver.com <haveagood@naver.com>
2020-06-11 00:01:53 +0900

    BE-#13 feat : Token을 활용하여 User정보 받아오는 기능 구현
    - User 정보를 가져오는데 값이 존재하지 않을 경우 일단 null값을 가져오도록 구현 (Custom Exception으로 변경 예정)
    - User 정보에서 email이 존재하지 않을 경우 Github에 email값을 다시 요청하여 email값을 넣도록 구현

--- BE/src/main/java/kr/codesquad/issuetracker/service/LoginService.java
+++ BE/src/main/java/kr/codesquad/issuetracker/service/LoginService.java
@@ -1,10 +1,12 @@
 package kr.codesquad.issuetracker.service;
 
 import kr.codesquad.issuetracker.common.security.GithubKey;
+import kr.codesquad.issuetracker.domain.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
@@ -14,9 +16,11 @@ public class LoginService {
 
     private static final Logger log = LoggerFactory.getLogger(LoginService.class);
     private final GithubKey githubKey;
+    private final OAuthService authService;
 
-    public LoginService(GithubKey githubKey) {
+    public LoginService(GithubKey githubKey, OAuthService authService) {
         this.githubKey = githubKey;
+        this.authService = authService;
     }
 
     public HttpHeaders redirectToGithub() {
@@ -28,6 +32,13 @@ public class LoginService {
                 .toUri();
         headers.setLocation(uri);
         return headers;
+    }
+
+    @Transactional
+    public User insertUser(String token) {
+        User user = authService.getUserInfoToToken(token);
+        log.info("User Info : {}", user);
+        return user;
     }
 
 }
--- BE/src/main/java/kr/codesquad/issuetracker/service/OAuthService.java
+++ BE/src/main/java/kr/codesquad/issuetracker/service/OAuthService.java
@@ -4,11 +4,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import kr.codesquad.issuetracker.common.security.GithubKey;
 import kr.codesquad.issuetracker.common.security.GithubPayload;
 import kr.codesquad.issuetracker.common.security.GithubToken;
+import kr.codesquad.issuetracker.common.security.GithubUser;
+import kr.codesquad.issuetracker.domain.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
 
 @Service
 public class OAuthService {
@@ -35,4 +41,33 @@ public class OAuthService {
                 .getBody();
     }
 
+    public User getUserInfoToToken(String token) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "token " + token);
+        GithubUser user = Optional.ofNullable(
+                restTemplate
+                        .exchange(GITHUB_USER_API_URL, HttpMethod.GET, new HttpEntity<>(headers), GithubUser.class)
+                        .getBody())
+                .orElse(null);
+
+        log.info("user info from github: {}", user);
+
+        if (user.getEmail() == null) {
+            user.setEmail(getEmailFromGithub(headers));
+        }
+        return User.builder()
+                .nickName(user.getName())
+                .email(user.getEmail())
+                .githubToken(token)
+                .build();
+    }
+
+    private String getEmailFromGithub(HttpHeaders headers) {
+        String eamil = restTemplate
+                .exchange(GITHUB_USER_API_URL+"/emails",HttpMethod.GET,new HttpEntity<>(headers),String.class)
+                .getBody();
+
+        log.info("email info : {}",eamil);
+        return eamil;
+    }
 }
