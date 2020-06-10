9ee72a3a876602b054e63123105973af3a33f9fa
IdionKim <ksundong@gmail.com>
2020-06-10 20:27:27 +0900

    BE-#18 test: User domain 객체 TDD로 개발

---
+++ BE/src/main/java/kr/codesquad/issuetracker/domain/User.java
@@ -0,0 +1,22 @@
+package kr.codesquad.issuetracker.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    private Long id; // TODO: JPA 연동시 Auto Increment 적용
+    private String userId;
+    private String profileImage;
+
+    @Builder
+    public User(Long id, String userId, String profileImage) {
+        this.id = id;
+        this.userId = userId;
+        this.profileImage = profileImage;
+    }
+}
