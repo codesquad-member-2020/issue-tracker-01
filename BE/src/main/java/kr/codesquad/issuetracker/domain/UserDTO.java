284d2e37c03907d6441fa0a844be15c5ea5f9e1e
haveagood@naver.com <haveagood@naver.com>
2020-06-10 20:11:08 +0900

    BE-#13 feat : User 정보를 담는 DTO 클래스 추가
    - User 정보를 담을 DTO 클래스 추가

---
+++ BE/src/main/java/kr/codesquad/issuetracker/domain/UserDTO.java
@@ -0,0 +1,27 @@
+package kr.codesquad.issuetracker.domain;
+
+import java.util.Map;
+
+public class UserDTO {
+
+    private final String nickName;
+    private final String eamil;
+
+    private UserDTO(String nickName, String eamil) {
+        this.nickName = nickName;
+        this.eamil = eamil;
+    }
+
+    private UserDTO(Map<String, String> userMap) {
+        this.nickName = userMap.get("nickName");
+        this.eamil = userMap.get("email");
+    }
+
+    public static UserDTO of(String nickName, String eamil) {
+        return new UserDTO(nickName, eamil);
+    }
+
+    public static UserDTO of(Map<String, String> userMap) {
+        return new UserDTO(userMap);
+    }
+}
