b76c0ab01f5c4563d0dbcc1a654edfbac93c7d90
haveagood@naver.com <haveagood@naver.com>
2020-06-10 20:17:07 +0900

    BE-#13 feat : Login 기능 구현
    - Github OAuth에 client_id, client_secret을 통해서 로그인 하려는 User의 Token을 받아오도록 구현
    - 해당 Token을 통해서 User의 정보를 받아와서 Token을 화면에 출력하도록 구현(Token으로 해당 User의 정보를 가져오도록 할 예정)

--- BE/src/main/java/kr/codesquad/issuetracker/controller/LoginController.java
+++ BE/src/main/java/kr/codesquad/issuetracker/controller/LoginController.java
@@ -1,12 +1,15 @@
 package kr.codesquad.issuetracker.controller;
 
 import io.swagger.annotations.Api;
+import kr.codesquad.issuetracker.service.JwtService;
+import kr.codesquad.issuetracker.service.LoginService;
+import kr.codesquad.issuetracker.service.OAuthService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Api(tags = "Login")
 @RestController
@@ -14,14 +17,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
 
     private static final Logger log = LoggerFactory.getLogger(LoginController.class);
+    private final OAuthService authService;
+    private final JwtService jwtService;
+    private final LoginService loginService;
+
+    public LoginController(OAuthService authService, JwtService jwtService, LoginService loginService) {
+        this.authService = authService;
+        this.jwtService = jwtService;
+        this.loginService = loginService;
+    }
 
     @GetMapping
-    public ResponseEntity<String> loginWithGithub() {
-        return new ResponseEntity<>();
+    public ResponseEntity<String> loginWithGithub(@CookieValue(value = "jwt", required = false) String jwt) {
+        if (jwt != null) {
+            log.debug("jwt token : {}", jwt);
+            log.debug("jwt token value : {}", jwtService.getUserFromJws(jwt));
+            return ResponseEntity.ok(jwtService.getUserFromJws(jwt).toString());
+        }
+        HttpHeaders headers = loginService.redirectToGithub();
+        return new ResponseEntity<>("redirect", headers, HttpStatus.SEE_OTHER);
     }
 
     @GetMapping("/oauth")
-    public ResponseEntity<String> oauthAuthentication() {
-        return new ResponseEntity<>();
+    public ResponseEntity<String> oauthAuthentication(@RequestParam("code") String code) {
+        String accessToken = authService.getTokenFromCode(code).getAccessToken();
+        return new ResponseEntity<>(accessToken, HttpStatus.OK);
     }
 }
