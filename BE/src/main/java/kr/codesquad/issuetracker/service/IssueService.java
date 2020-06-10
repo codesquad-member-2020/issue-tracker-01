dca014b5ee139ae20e03eea0b05870dfcd062cb8
IdionKim <ksundong@gmail.com>
2020-06-10 18:30:39 +0900

    BE-#18 test: IssueControllerTest에 결과 필터하는 부분 추가
    조회 비즈니스 로직을 담당하는 IssueService 클래스를 생성하였습니다.

---
+++ BE/src/main/java/kr/codesquad/issuetracker/service/IssueService.java
@@ -0,0 +1,13 @@
+package kr.codesquad.issuetracker.service;
+
+import kr.codesquad.issuetracker.domain.Issue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IssueService {
+
+    public List<Issue> findOpenedIssues() {
+        return new ArrayList<>();
+    }
+}
--- BE/src/test/java/kr/codesquad/issuetracker/controller/IssueControllerTest.java
+++ BE/src/test/java/kr/codesquad/issuetracker/controller/IssueControllerTest.java
@@ -1,17 +1,28 @@
 package kr.codesquad.issuetracker.controller;
 
 import kr.codesquad.issuetracker.domain.Issue;
+import kr.codesquad.issuetracker.service.IssueService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.mockito.Mockito.when;
 
 @WebMvcTest(controllers = {IssueController.class})
 public class IssueControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    MockMvc mockMvc;
+
+    @MockBean
+    IssueService issueService;
 
     @Test
     @DisplayName("오픈된 이슈 조회 검색 테스트")
@@ -19,6 +30,10 @@ public class IssueControllerTest {
         // given
         Issue issue1 = Issue.builder().isOpened(true).build();
         Issue issue2 = Issue.builder().isOpened(false).build();
+        Issue issue3 = Issue.builder().isOpened(true).build();
+
+        List<Issue> issues = Stream.of(issue1, issue2, issue3).filter(Issue::isOpened).collect(Collectors.toList());
+        when(issueService.findOpenedIssues()).thenReturn(issues);
 
         // then
     }
