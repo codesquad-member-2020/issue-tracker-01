b99a542c0f63621b8918c3305f8fa1b8a044fac1
IdionKim <ksundong@gmail.com>
2020-06-10 18:42:06 +0900

    BE-#18 fix: 누락된 어노테이션 추가

--- BE/src/main/java/kr/codesquad/issuetracker/service/IssueService.java
+++ BE/src/main/java/kr/codesquad/issuetracker/service/IssueService.java
@@ -1,10 +1,14 @@
 package kr.codesquad.issuetracker.service;
 
 import kr.codesquad.issuetracker.domain.Issue;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
+@Service
 public class IssueService {
 
     public List<Issue> findOpenedIssues() {
