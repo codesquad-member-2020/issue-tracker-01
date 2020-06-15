26dd6db79cea555cba339120506d2b2b205e0e60
haveagood@naver.com <haveagood@naver.com>
2020-06-15 14:00:26 +0900

    BE-#13 refactor : review를 참고하여 코드 수정
    - 하드 코딩 제거

--- BE/src/main/java/kr/codesquad/issuetracker/service/LoginService.java
+++ BE/src/main/java/kr/codesquad/issuetracker/service/LoginService.java
@@ -1,11 +1,9 @@
 package kr.codesquad.issuetracker.service;
 
 import kr.codesquad.issuetracker.common.security.GithubKey;
-import kr.codesquad.issuetracker.domain.User;
-import kr.codesquad.issuetracker.domain.dto.UserDTO;
+import kr.codesquad.issuetracker.domain.entity.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
@@ -19,13 +17,15 @@ import java.net.URI;
 public class LoginService {
 
     private static final int MAX_AGE = 7 * 24 * 60 * 60;
-    private static final String ROOT_DIR = "https://github.com/login/oauth/authorize";
+    public static final URI ROOT_URL = URI.create("http://localhost:8080");
+    private static final String AUTHORIZE_URL = "https://github.com/login/oauth/authorize";
+
     private final OAuthService authService;
     private final GithubKey githubKey;
 
     public HttpHeaders redirectToGithub() {
         HttpHeaders headers = new HttpHeaders();
-        URI uri = UriComponentsBuilder.fromUriString(ROOT_DIR)
+        URI uri = UriComponentsBuilder.fromUriString(AUTHORIZE_URL)
                 .queryParam("client_id", githubKey.getClientId())
                 .queryParam("scope", "user")
                 .build()
@@ -40,16 +40,12 @@ public class LoginService {
         return user;
     }
 
-    public UserDTO createUserDTO(User user) {
-        return UserDTO.of(user.getNickname(), user.getEmail());
-    }
-
     public HttpHeaders redirectWithCookie(String jwt) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setBearerAuth(jwt);
         headers.add(HttpHeaders.SET_COOKIE, "jwt=" + jwt + "; Path=/" + "; Max-Age=" + MAX_AGE + ";");
-        headers.setLocation(URI.create("http://localhost:8080"));
+        headers.setLocation(ROOT_URL);
         return headers;
     }
 
