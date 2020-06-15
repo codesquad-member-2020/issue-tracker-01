3992653fc2396f587236ad85b614d1008b5669ec
haveagood@naver.com <haveagood@naver.com>
2020-06-15 14:00:42 +0900

    BE-#13 refactor : review를 참고하여 코드 수정
    - 오타 수정

--- BE/src/main/java/kr/codesquad/issuetracker/service/OAuthService.java
+++ BE/src/main/java/kr/codesquad/issuetracker/service/OAuthService.java
@@ -8,13 +8,9 @@ import kr.codesquad.issuetracker.common.security.GithubKey;
 import kr.codesquad.issuetracker.common.security.GithubPayload;
 import kr.codesquad.issuetracker.common.security.GithubToken;
 import kr.codesquad.issuetracker.common.security.GithubUser;
-import kr.codesquad.issuetracker.domain.User;
-import lombok.NoArgsConstructor;
+import kr.codesquad.issuetracker.domain.entity.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -27,8 +23,10 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class OAuthService {
+
     private static final String GITHUB_ACCESS_TOKEN_URL = "https://github.com/login/oauth/access_token";
     private static final String GITHUB_USER_API_URL = "https://api.github.com/user";
+
     private final GithubKey githubKey;
 
     public GithubToken getTokenFromCode(String code) {
@@ -60,13 +58,13 @@ public class OAuthService {
     }
 
     private String getEmailFromGithub(HttpHeaders headers) {
-        String eamil = new RestTemplate()
+        String email = new RestTemplate()
                 .exchange(GITHUB_USER_API_URL + "/emails", HttpMethod.GET, new HttpEntity<>(headers), String.class)
                 .getBody();
 
-        log.info("email info : {}", eamil);
+        log.info("email info : {}", email);
         try {
-            JsonNode emailNode = new ObjectMapper().readTree(eamil);
+            JsonNode emailNode = new ObjectMapper().readTree(email);
             for (JsonNode jsonNode : emailNode) {
                 log.info("JsonNode : {}", jsonNode);
                 if (jsonNode.get("primary").asBoolean()) {
