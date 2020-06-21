a19cd3f51757b9c940eb6025cf181fd193db73cf
IdionKim <ksundong@gmail.com>
2020-06-21 19:40:41 +0900

    BE-#62 test: UserRepositoryTest 추가

---
+++ BE/src/test/java/kr/codesquad/issuetracker/domain/repository/UserRepositoryTest.java
@@ -0,0 +1,27 @@
+package kr.codesquad.issuetracker.domain.repository;
+
+import kr.codesquad.issuetracker.domain.entity.User;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.Transactional;
+
+@DataJpaTest
+@ComponentScan({"kr.codesquad.issuetracker.domain"})
+class UserRepositoryTest {
+
+  @Autowired
+  private UserRepository userRepository;
+
+  @Test
+  @DisplayName("save and find method Test")
+  @Transactional
+  void saveAndFindMethodTest() {
+    User user = User.builder().build();
+    Long id = userRepository.save(user);
+    Assertions.assertThat(userRepository.find(id)).isEqualTo(user);
+  }
+}
