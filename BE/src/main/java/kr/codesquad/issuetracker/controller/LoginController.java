dc222b578b2cd865345bdf841ac1f6055418293a
IdionKim <ksundong@gmail.com>
2020-06-22 12:40:48 +0900

    BE-#62 test: LoginControllerTest 추가

--- BE/src/main/java/kr/codesquad/issuetracker/controller/LoginController.java
+++ BE/src/main/java/kr/codesquad/issuetracker/controller/LoginController.java
@@ -28,12 +28,12 @@ public class LoginController {
   private final LoginService loginService;
 
   @GetMapping
-  public ResponseEntity<String> loginWithGithub(
+  public ResponseEntity<Object> loginWithGithub(
       @CookieValue(value = "jwt", required = false) String jwt) {
     if (jwt != null) {
       log.debug("jwt token : {}", jwt);
       log.debug("jwt token value : {}", jwtService.getUserFromJws(jwt));
-      return ResponseEntity.ok(jwtService.getUserFromJws(jwt).toString());
+      return ResponseEntity.ok(jwtService.getUserFromJws(jwt));
     }
     HttpHeaders headers = loginService.redirectToGithub();
     return new ResponseEntity<>("redirect", headers, HttpStatus.SEE_OTHER);
