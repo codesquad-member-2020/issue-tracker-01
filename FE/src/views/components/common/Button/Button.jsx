74a8041e60ff071138d26385a459c72b9da6c52e
Reese Kim <reesekimm@gmail.com>
2020-07-11 14:20:25 +0900

    FE-#32 fix: Button block 속성 적용 안되는 문제 해결
    block property를 Button 컴포넌트에 props로 전달하여 조건부 적용되도록 함

--- FE/src/views/components/common/Button/Button.jsx
+++ FE/src/views/components/common/Button/Button.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { StyledButton } from "./Button.js";
 
-const MyButton = ({ type, size, shape, text, bgcolor }) => {
+const MyButton = ({ type, size, shape, text, bgcolor, block }) => {
   return (
-    <StyledButton type={type} size={size} shape={shape} style={{ background: bgcolor }}>
+    <StyledButton
+      type={type}
+      size={size}
+      shape={shape}
+      style={{ background: bgcolor, width: block ? "100%" : "" }}
+    >
       {text}
     </StyledButton>
   );
