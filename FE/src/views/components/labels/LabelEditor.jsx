102363873eb9037c3598bc83d4f5b21cb5901325
Reese Kim <reesekimm@gmail.com>
2020-07-21 20:22:59 +0900

    FE-#153 feat: Color 버튼 클릭하여 Label color 변경하는 기능 추가
    - 현재는 버튼을 클릭해야만 색상 변경 가능 (read-only)
    - 입력창을 통한 색상 변경 기능은 추후 구현 예정

--- FE/src/views/components/labels/LabelEditor.jsx
+++ FE/src/views/components/labels/LabelEditor.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled, { css } from "styled-components";
 import Button from "@/common/Button";
 import Input from "@/common/Input";
 import Label from "@/common/Label";
 import { RetweetOutlined } from "@ant-design/icons";
+import { generateRandomColor } from "Utils/utilFunctions";
 
 const temp = {
   id: null,
@@ -12,19 +13,24 @@ const temp = {
   description: null,
 };
 
-const LabelEditor = ({ mode, values, handlers }) => {
-  const [editing, setEditing] = useState(false);
-  const { id, title, color, description } = values || temp;
+const buttons = {
+  create: "Create label",
+  list: "Save changes",
+};
 
-  const buttons = {
-    create: "Create label",
-    list: "Save changes",
-  };
+const LabelEditor = ({ mode, values = temp, handlers }) => {
+  const { id, title, color, description } = values;
+  const [editing, setEditing] = useState(false);
+  const [labelColor, setLabelColor] = useState(color);
+
+  useEffect(() => {
+    if (!color) setLabelColor(generateRandomColor());
+  }, []);
 
   return (
     <Wrapper mode={mode}>
       <PreviewWrapper>
-        <Label title={title || "Label preview"} color={color || "#7057FF"} />
+        <Label title={title || "Label preview"} color={labelColor} />
         {mode === "list" && (
           <>
             {!editing && <Description>{description}</Description>}
@@ -39,19 +45,19 @@ const LabelEditor = ({ mode, values, handlers }) => {
         <EditorWrapper>
           <InputWrapper>
             <StyledLabel>Label name</StyledLabel>
-            <Input placeholder="Label name" defaultValue={title} />
+            <Input placeholder="Label name" value={title} />
           </InputWrapper>
           <InputWrapper>
             <StyledLabel>Description</StyledLabel>
-            <Input placeholder="Description (optional)" defaultValue={description} />
+            <Input placeholder="Description (optional)" value={description} />
           </InputWrapper>
           <InputWrapper>
             <StyledLabel>Color</StyledLabel>
             <ColorWrapper>
-              <Refresh>
+              <Refresh color={labelColor} onClick={() => setLabelColor(generateRandomColor())}>
                 <RetweetOutlined style={{ color: "#fff", fontWeight: "bold" }} />
               </Refresh>
-              <Input style={{ flex: "1" }} defaultValue={color} />
+              <Input style={{ flex: "1" }} value={labelColor} />
             </ColorWrapper>
           </InputWrapper>
           <ButtonWrapper>
@@ -127,7 +133,7 @@ const Refresh = styled.div`
   width: 32px;
   height: 32px;
   border-radius: 5px;
-  background: #c6ddff;
+  background: ${(props) => props.color};
   display: flex;
   justify-content: center;
   align-items: center;
