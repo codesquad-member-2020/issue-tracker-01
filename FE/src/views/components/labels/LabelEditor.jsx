3a1cd53764a792a6ffc5c8d98b47adade5780c95
Reese Kim <reesekimm@gmail.com>
2020-07-26 15:19:29 +0900

    FE-#153 refactor: LabelEditor useReducer 코드 리팩토링
    action creator를 컴포넌트 내부에 선언하고 dispatch 함수와 바인딩하여 action 생성과 동시에 dispatch 할 수 있도록 함 (bound action creator로 변형)

--- FE/src/views/components/labels/LabelEditor.jsx
+++ FE/src/views/components/labels/LabelEditor.jsx
@@ -14,26 +14,12 @@ const initialValues = {
   description: "",
 };
 
-/* Action Types */
-
 const inputActions = {
   UPDATE_TITLE: "UPDATE_TITLE",
   UPDATE_DESCRIPTION: "UPDATE_DESCRIPTION",
   UPDATE_COLOR: "UPDATE_COLOR",
   RESET_VALUES: "RESET_VALUES",
 };
-
-/* Action Creators */
-
-const updateTitle = (title) => ({ type: inputActions.UPDATE_TITLE, payload: title });
-const updateDescription = (description) => ({
-  type: inputActions.UPDATE_DESCRIPTION,
-  payload: description,
-});
-const updateColor = (color) => ({ type: inputActions.UPDATE_COLOR, payload: color });
-const resetValues = () => ({ type: inputActions.RESET_VALUES });
-
-/* Reducer */
 
 const inputReducer = (state, { type, payload }) => {
   const { UPDATE_TITLE, UPDATE_DESCRIPTION, UPDATE_COLOR, RESET_VALUES } = inputActions;
@@ -60,12 +46,22 @@ const LabelEditor = ({ mode, values, handlers }) => {
   const [editing, setEditing] = useState(false);
   const [inputValues, inputDispatch] = useReducer(inputReducer, values || initialValues);
   const { id, title, color, description } = inputValues;
-
+  const { labelIdToDelete } = useSelector((state) => state.label);
   const { CREATE_LABEL, DELETE_LABEL } = useSelector((state) => state.loading);
 
   useEffect(() => {
-    if (!color) inputDispatch(updateColor(generateRandomColor()));
+    if (!color) updateColor(generateRandomColor());
   }, []);
+
+  const updateTitle = (title) => inputDispatch({ type: inputActions.UPDATE_TITLE, payload: title });
+  const updateDescription = (description) =>
+    inputDispatch({
+      type: inputActions.UPDATE_DESCRIPTION,
+      payload: description,
+    });
+  const updateColor = (labelColor) =>
+    inputDispatch({ type: inputActions.UPDATE_COLOR, payload: labelColor });
+  const resetValues = () => inputDispatch({ type: inputActions.RESET_VALUES });
 
   return (
     <Wrapper mode={mode}>
@@ -76,12 +72,7 @@ const LabelEditor = ({ mode, values, handlers }) => {
             {!editing && <Description>{description}</Description>}
             <ButtonWrapper>
               {!editing && <Button type="text" text="Edit" onClick={() => setEditing(true)} />}
-              <Button
-                type="text"
-                text="Delete"
-                onClick={() => handlers.onClickDelete(id)}
-                // loading={id && DELETE_LABEL}
-              />
+              <Button type="text" text="Delete" onClick={() => handlers.onClickDelete(id)} />
             </ButtonWrapper>
           </>
         )}
@@ -93,7 +84,7 @@ const LabelEditor = ({ mode, values, handlers }) => {
             <Input
               placeholder="Label name"
               value={title}
-              onChange={(e) => inputDispatch(updateTitle(e.target.value))}
+              onChange={(e) => updateTitle(e.target.value)}
             />
           </InputWrapper>
           <InputWrapper>
@@ -101,16 +92,13 @@ const LabelEditor = ({ mode, values, handlers }) => {
             <Input
               placeholder="Description (optional)"
               value={description}
-              onChange={(e) => inputDispatch(updateDescription(e.target.value))}
+              onChange={(e) => updateDescription(e.target.value)}
             />
           </InputWrapper>
           <InputWrapper>
             <StyledLabel>Color</StyledLabel>
             <ColorWrapper>
-              <Refresh
-                color={color}
-                onClick={() => inputDispatch(updateColor(generateRandomColor()))}
-              >
+              <Refresh color={color} onClick={() => updateColor(generateRandomColor())}>
                 <RetweetOutlined style={{ color: "#fff", fontWeight: "bold" }} />
               </Refresh>
               <Input style={{ flex: "1" }} value={color} readOnly />
@@ -126,7 +114,7 @@ const LabelEditor = ({ mode, values, handlers }) => {
               text={buttons[mode]}
               onClick={async () => {
                 await handlers.onClickCreate(inputValues);
-                inputDispatch(resetValues());
+                resetValues();
               }}
               loading={CREATE_LABEL}
             />
