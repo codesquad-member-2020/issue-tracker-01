dcd0f9c7e2f2d4a59950bb354801a257a127c5e4
Reese Kim <reesekimm@gmail.com>
2020-07-28 17:05:22 +0900

    FE-#160 fix: markdown style 반영 안되는 이슈 해결
    CommentEditor 컴포넌트 내부에서 markdown 미리보기시 styled-reset에 의해 기본 엘리먼트 스타일이 반영되지 않는 이슈 발생
    
    -> GlobalStyle에 styled-reset 적용 안함
    -> ul 컴포넌트에 list-style: none 적용

--- FE/src/styles/GlobalStyle.js
+++ FE/src/styles/GlobalStyle.js
@@ -1,12 +1,9 @@
 import { createGlobalStyle } from "styled-components";
-import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
-  ${reset}
   * {
     box-sizing: border-box;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji' !important;
-    font-size: 16px;
     &:before, &:after {
       box-sizing: border-box;
     }
